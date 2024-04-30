f85e13c3c3b0ecbfa42346cc127c0949bec1bc45
Via Liu <bangchel@sas.upenn.edu>
2024-04-30 13:47:11 -0400

    added popup.html and changed background/manifest to fix bugs

--- extension-draft/public/background.js
+++ extension-draft/public/background.js
@@ -1,20 +1,24 @@
 // This function is called when the extension is installed or updated
 chrome.runtime.onInstalled.addListener(() => {
   // Create a context menu item
-  // See: https://developer.chrome.com/docs/extensions/reference/api/contextMenus#method-create
   chrome.contextMenus.create({
-    id: "exampleContextMenu", // Unique identifier for the context menu item
-    title: "Context Menu", // Text to be displayed in the context menu
-    contexts: ["selection"], // Show the context menu item only when text is selected
+    id: "exampleContextMenu",
+    title: "Context Menu",
+    contexts: ["selection"],
   });
 });
 
 // This function is called when a context menu item is clicked
-// See: https://developer.chrome.com/docs/extensions/reference/api/contextMenus#event-onClicked
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  // Check if the clicked menu item is 'exampleContextMenu'
   if (info.menuItemId === "exampleContextMenu") {
-    const selectedText = info.selectionText; // Get the selected text
+    const selectedText = info.selectionText;
     console.log(Date.now(), "Selected text: ", selectedText);
+  }
+});
+
+// Open the landing page when the "Details" button is clicked in the popup
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "openLandingPage") {
+    chrome.tabs.create({ url: "index.html" });
   }
 });
