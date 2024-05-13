684a8d21bb108386eb21a129759e7cf0acd52ede
Via Liu <bangchel@sas.upenn.edu>
2024-05-13 08:17:40 -0400

    deleted redundant files

--- src/popup.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const detailsButton = document.getElementById('detailsButton');
-  detailsButton.addEventListener('click', function() {
-      console.log("Sending message to open landing page");
-      chrome.runtime.sendMessage({action: "openLandingPage"});
-  });
-});
